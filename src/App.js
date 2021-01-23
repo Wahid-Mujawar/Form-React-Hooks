230e67be8836102efcd4f12e101d017095a4048e
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-22 22:32:45 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 function FormUsingHook() {
+
+  const [values, setValues] = useState({
+    firstName="",
+    lastName="",
+    email="",
+  });
 
   return (
     <div className="form-container">
       <form className="register-form">
         <input
-          className = "form-field">
+          className = "form-field"
           placeholder ="First Name"
-          name="firstname"
+          name="firstname">
         </input>
 
         <input
-          className = "form-field">
+          className = "form-field"
           placeholder ="Last Name"
-          name="lastname"
+          name="lastname">
         </input>
 
         <input
-          className = "form-field">
+          className = "form-field"
           placeholder ="Email"
-          name="email"
+          name="email">
         </input>
+
+        <button 
+        className = "form-field"
+        type="submit">Register
+        </button>
       </form>
-     
     </div>
   );
 }
