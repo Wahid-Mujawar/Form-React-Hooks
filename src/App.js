dcf71398fe94d33ebae3c078489a83ee47a36b54
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-22 21:40:06 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import React from 'react'
 
-function App() {
+function FormUsingHook() {
+
   return (
-    <div className="App">
-      Wahid
+    <div className="form-container">
+      <form className="register-form">
+        <input
+          className = "form-field">
+          placeholder ="First Name"
+          name="firstname"
+        </input>
+
+        <input
+          className = "form-field">
+          placeholder ="Last Name"
+          name="lastname"
+        </input>
+
+        <input
+          className = "form-field">
+          placeholder ="Email"
+          name="email"
+        </input>
+      </form>
+     
     </div>
   );
 }
 
-export default App;
+export default FormUsingHook;
